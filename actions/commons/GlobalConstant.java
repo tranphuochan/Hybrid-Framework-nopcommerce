7f80e681bebfa1e5ad32bdef3e752efe23683b04
TranPhuocHan <tranphuochan@gmail.com>
2022-09-10 15:47:01 +0700

    Framework

---
+++ actions/commons/GlobalConstant.java
@@ -0,0 +1,13 @@
+package commons;
+
+public class GlobalConstant {
+	// dùng cho toàn bộ tất cả các class khác trong Frw có thể truy cập được
+	// Biến dùng chung
+	// User/Pass -> app
+	// User/Pass -> Database
+	// Database
+	// Server
+	// Environment
+	// ...
+
+}
---
+++ testcases/com/nopcommerce/users/User_01_Register.java
@@ -0,0 +1,26 @@
+package com.nopcommerce.users;
+
+import org.testng.annotations.Test;
+
+public class User_01_Register {
+	@Test(groups= {"regresstion","user"})
+	public void Register_01_Empty_Data() {
+		
+
+	}
+	@Test
+	public void Register_02_Email_Incorrect() {
+		
+
+	}
+	@Test
+	public void Register_03_Email_Exist() {
+		
+
+	}
+	@Test
+	public void Register_04_Password_Not_Match() {
+		
+
+	}
+}
